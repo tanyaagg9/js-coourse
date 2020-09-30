d9dd0063fcfe25137b5bce72a510528e60faad63
vaibhav-mandhani <56543822+vaibhav-mandhani@users.noreply.github.com>
2020-10-01 01:05:38 +0530

    Create milisecond.js

---
+++ milisecond.js
@@ -0,0 +1,17 @@
+<html>  
+<head>  
+  
+</head>  
+<body>  
+<h1> Hello World :) :) </h1>  
+<p> Here, we are finding the number of milliseconds between the given date and midnight of January 1, 1970. </p>  
+  
+<script>  
+var d1 = "June 19, 2020";  
+var m1 = Date.parse(d1);  
+document.write("The number of milliseconds between <b> " + d1 + "</b> and <b> January 1, 1970 </b> is: <b> " + m1 + "</b>");  
+</script>  
+  
+</body>  
+  
+</html> 
